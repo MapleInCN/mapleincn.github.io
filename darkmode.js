d39ddf6d2083cd59c2ed52fec8a81d6312980769
Maple <95948351+MapleInCN@users.noreply.github.com>
2023-01-05 22:18:09 +0800

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,14 +1,14 @@
-function darkMode(){
- var dark = document.cookie.split(";")[0].split("=")[1]; 
- var d = new Date();
- d.setHours(d.getHours() + (24 * 30)); 
- if(dark == '1'){
-  document.getElementById('dark').innerHTML = '';
-  document.getElementById('night').innerHTML = '<i class="fa-solid fa-moon"></i>深色';
-  document.cookie = "dark= 0; path=/; expires="+d.toGMTString();
- }else{
-  document.getElementById('dark').innerHTML = '@media {html,img,iframe,after { filter: invert(1) hue-rotate(180deg); }}';
-  document.getElementById('night').innerHTML = '<i class="fa-solid fa-lightbulb"></i>浅色';
-  document.cookie = "dark= 1; path=/; expires="+d.toGMTString();
- }
+function darkMode() {
+var darkMode = document.cookie.split(";")[0].split("=")[1]; 
+var d = new Date();
+d.setHours(d.getHours() + (24 * 30)); 
+if(darkMode == 'on'){
+document.body.classList.remove( 'dark-mode' );
+document.getElementById('dark-toggler').innerHTML = '<i class="fa-solid fa-moon"></i>深色';
+document.cookie = "darkMode = off; path=/; expires="+d.toGMTString();
+}else{
+document.body.classList.add( 'dark-mode' );
+document.getElementById('dark-toggler').innerHTML = '<i class="fa-solid fa-lightbulb"></i>浅色';
+document.cookie = "darkMode = on; path=/; expires="+d.toGMTString();
+}
 }
