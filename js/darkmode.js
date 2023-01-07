628299e0a1a9870593170d36c654ecde6366403e
Maple <95948351+MapleInCN@users.noreply.github.com>
2023-01-08 03:16:13 +0800

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,24 +1,22 @@
-var l = '<i class="fa-solid fa-lightbulb"></i>浅色';
-var d = '<i class="fa-solid fa-moon"></i>深色';
-var toggler = document.getElementById('dark-toggler');
 $(function() {
   const darkMode = localStorage.getItem("darkMode");
   if (darkMode === 'on') {
-    toggler.innerHTML = l;
-    $("body").addClass("dark-mode")
+    document.getElementById('dark-toggler').innerHTML = '<i class="fa-solid fa-lightbulb"></i>浅色';
+    $("body").addClass("dark-mode");
   } else {
-    toggler.innerHTML = d;
-    $("body").removeClass("dark")
+    document.getElementById('dark-toggler').innerHTML = '<i class="fa-solid fa-moon"></i>深色';
+    $("body").removeClass("dark");
   }
 });
+
 function darkSwitch() {
   $("body").toggleClass("dark-mode");
   const darkMode = localStorage.getItem("darkMode");
   if (darkMode === 'on') {
-    toggler.innerHTML = d;
+    document.getElementById('dark-toggler').innerHTML = '<i class="fa-solid fa-moon"></i>深色';
     localStorage.setItem("darkMode", "off")
   } else {
-    toggler.innerHTML = l;
+    document.getElementById('dark-toggler').innerHTML = '<i class="fa-solid fa-lightbulb"></i>浅色';
     localStorage.setItem("darkMode", "on")
   }
 }
